83af2c32b5622ee58033e5e63db40e57754f853e
costa92 <944310536@qq.com>
2025-02-16 22:10:26 +0800

    Implement Kubernetes pod informer with graceful HTTP server shutdown

--- main.go
+++ main.go
@@ -3,29 +3,92 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
 	config, err := clientcmd.BuildConfigFromFlags("", "/Users/costalong/.kube/config")
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error building kubeconfig: %v", err)
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error creating Kubernetes client: %v", err)
 	}
 
-	pods, err := clientset.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err)
+	// Create a shared informer factory
+	factory := informers.NewSharedInformerFactory(clientset, time.Minute)
+
+	// Create a pod informer
+	podInformer := factory.Core().V1().Pods().Informer()
+
+	// Add event handlers to the informer
+	podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			if pod, ok := obj.(*corev1.Pod); ok {
+				fmt.Printf("Pod added: %s\n", pod.Name)
+			}
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			if newPod, ok := newObj.(*corev1.Pod); ok {
+				fmt.Printf("Pod updated: %s\n", newPod.Name)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			if pod, ok := obj.(*corev1.Pod); ok {
+				fmt.Printf("Pod deleted: %s\n", pod.Name)
+			}
+		},
+	})
+
+	// Start the informer
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	go factory.Start(stopCh)
+
+	// Wait for the caches to be synced before starting the workers
+	if !cache.WaitForCacheSync(stopCh, podInformer.HasSynced) {
+		runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
+		return
 	}
 
-	for _, pod := range pods.Items {
-		fmt.Println(pod.Name)
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: http.DefaultServeMux,
 	}
+
+	// Graceful shutdown
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %v", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
